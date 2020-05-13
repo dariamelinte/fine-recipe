ff6b29c4e24fb3db3f541fb4602f9b8cdae12336
daria melinte <daria.melinte@cultofcoders.com>
2020-05-13 15:07:53 +0300

    +download recipe functionality done

--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -5,6 +5,7 @@ const updateRecipe = require('./updateRecipe')
 const deleteRecipe = require('./deleteRecipe')
 const addToFavorite = require('./addToFavorite')
 const addComment = require('./addComment')
+const downloadRecipe = require('./downloadRecipe')
 
 module.exports = {
   createRecipe,
@@ -13,5 +14,6 @@ module.exports = {
   updateRecipe,
   deleteRecipe,
   addToFavorite,
-  addComment
+  addComment,
+  downloadRecipe
 }
